c563beff4211a3a02886842a4b93722291420248
woshiyifadaguangtou <1826309@qq.com>
2016-12-05 23:22:10 +0800

    MyNg2项目搭建

---
+++ Venus.Test/MyNg2/wwwroot/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="../gulpfile.js" />
+/// <reference path="app/app.component.js" />
+/// <reference path="app/app.module.js" />
+/// <reference path="app/main.js" />
+/// <reference path="js/site.js" />
+/// <reference path="systemjs.config.js" />
